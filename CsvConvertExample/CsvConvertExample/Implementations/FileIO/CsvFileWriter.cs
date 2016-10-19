c498a9c2d71dd090307a7653e809f0b660dedc02
Md. Alim Ul Karim <devorg.bd@gmail.com>
2016-10-19 20:11:33 +0600

    Mutex file writer implemented with clean up

--- CsvConvertExample/CsvConvertExample/Implementations/FileIO/CsvFileWriter.cs
+++ CsvConvertExample/CsvConvertExample/Implementations/FileIO/CsvFileWriter.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using CsvConvertExample.Interfaces;
 
@@ -11,18 +13,29 @@ namespace CsvConvertExample.Implementations.FileIO
     {
         #region Implementation of IFileWriter
 
+        /// <summary>
+        /// Handle file writing using mutex and always overwrite.
+        /// </summary>
+        /// <param name="filepath">Give absolute file path. Throws exception if path is not valid.</param>
+        /// <param name="content">Pass string content which should be written in file.</param>
+        /// <returns>Returns true if writing is successful.</returns>
         public bool Write(string filepath, string content)
         {
             try
             {
-
+                using (var mutex = new Mutex(false, "CSV-Processor" + filepath))
+                {
+                    mutex.WaitOne();
+                    File.WriteAllText(filepath, content);
+                    mutex.ReleaseMutex();
+                }
             }
             catch (Exception ex)
             {
-
                 throw ex;
                 return false;
             }
+
             return true;
         }
 

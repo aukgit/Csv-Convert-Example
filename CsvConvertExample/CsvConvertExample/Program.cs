fcb67fd6366a11f9265c05af8b45b787c7506d24
Md. Alim Ul Karim <devorg.bd@gmail.com>
2016-10-19 22:41:03 +0600

    binding complete

--- CsvConvertExample/CsvConvertExample/Program.cs
+++ CsvConvertExample/CsvConvertExample/Program.cs
@@ -1,6 +1,7 @@
 ﻿using CsvConvertExample.DataLayer;
 using CsvConvertExample.Implementations.FileIO;
 using CsvConvertExample.Implementations.Formatters;
+using CsvConvertExample.Implementations.OrderFilters;
 using CsvConvertExample.Interfaces.FileIO;
 using CsvConvertExample.Interfaces.Formatter;
 using CsvConvertExample.Interfaces.OrderFilters;
@@ -14,9 +15,14 @@ namespace CsvConvertExample
             var kernal = new StandardKernel();
             kernal.Bind<ICsvReader<Person>>().To<CsvReaderForPeople>();
             kernal.Bind<IFileWriter>().To<CsvFileWriter>();
+            
             kernal.Bind<IPersonNameFrequencyFormatterForCsv<PeopleOrderByNameFrequency>>().To<PersonNameFrequencyFormatterForCsv>();
             kernal.Bind<IPersonStreetAddressFormatterForCsv<Person>>().To<PersonStreetAddressFormatterForCsv>();
-            kernal.Bind<IOrderFilterByName<Person>>().To<PersonStreetAddressFormatterForCsv>(); 
+            
+            kernal.Bind<IOrderFilterByName<Person, PeopleOrderByNameFrequency>>().To<PersonNameFrequencyFilter>();
+            kernal.Bind<IOrderFilterByAddress<Person, Person>>().To<PersonAddressOrderFilter>();
+            
+            kernal.Bind<IOrderFilterByAddress<Person, Person>>().To<PersonAddressOrderFilter>(); 
         }
     }
 }

df39f56f20f376d0a02546daaf52a02e8fa0b824
Md. Alim Ul Karim <devorg.bd@gmail.com>
2016-10-20 00:50:15 +0600

    Messages changed

--- CsvConvertExample/CsvConvertExample/Program.cs
+++ CsvConvertExample/CsvConvertExample/Program.cs
@@ -38,19 +38,19 @@ namespace CsvConvertExample
             var csvNameFrequencyFilePath = relativeFolder + @"output\name-frequency.csv";
             var csvStreetFilterFilePath = relativeFolder + @"output\street-address-filtered-alphabetically.csv";
 
-            Console.WriteLine("- Reading person data from csv : " + csvReadingPath);
+            Console.WriteLine("- [Start] Reading person data from csv : " + csvReadingPath);
             var people = csvProcessor.ReadCsv(csvReadingPath);
-            Console.WriteLine("- \"" + csvReadingPath + "\" file read successfully. We have total " + people.Count + " people.");
+            Console.WriteLine("- [Done] \"" + csvReadingPath + "\" file read successfully. We have total " + people.Count + " people.");
 
             // TODO : Also should have used a logger.
             // TODO : Could have refactor this to Action based delegates.
-            Console.WriteLine("- [Start] filtering : sorting first and last by frequency of descending and then alphabetically");
+            Console.WriteLine("- [Start] filtering : sorting first and last by frequency of descending and then alphabetically.");
             var nameFrequencyFilteredResults = csvProcessor.OrderFilterByName(people);
-            Console.WriteLine("- [Done] filtering : sorting first and last by frequency of descending and then alphabetically");
+            Console.WriteLine("- [Done] filtering : sorting first and last by frequency of descending and then alphabetically.");
             var nameFrequencyFormatterForCsv = container.Get<PersonNameFrequencyFormatterForCsv>();
-            Console.WriteLine("- [Start] writing csv : " + csvNameFrequencyFilePath);
+            Console.WriteLine("- [Start] writing csv : " + csvNameFrequencyFilePath + ".");
             csvProcessor.WriteAsCsvFile(nameFrequencyFilteredResults, nameFrequencyFormatterForCsv, csvNameFrequencyFilePath);
-            Console.WriteLine("- [Done] writing csv : " + csvNameFrequencyFilePath);
+            Console.WriteLine("- [Done] writing csv : " + csvNameFrequencyFilePath + ".");
 
             Console.WriteLine("- [Start] filtering : sorting address street name alphabetically.");
             var streetAddressFilteredAlphabeticallyResults = csvProcessor.OrderFilterByAddress(people);
@@ -58,8 +58,8 @@ namespace CsvConvertExample
             var streetAddressFormatterForCsv = container.Get<PersonStreetAddressFormatterForCsv>();
             Console.WriteLine("- [Start] writing csv : " + csvStreetFilterFilePath);
             csvProcessor.WriteAsCsvFile(streetAddressFilteredAlphabeticallyResults, streetAddressFormatterForCsv, csvStreetFilterFilePath);
-            Console.WriteLine("- [Done] writing csv : " + csvStreetFilterFilePath);
-            Console.WriteLine("- All operations are complete. Now press any key to exit.");
+            Console.WriteLine("- [Done] writing csv : " + csvStreetFilterFilePath + ".");
+            Console.WriteLine("- All operations are completed. Now press any key to exit.");
 
             Console.ReadKey();
         }
